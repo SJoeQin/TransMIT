4c5ab4ea8b6106094bcab7fcf5a3e4bc393e41f9
SJoeQin <sjoeqin@outlook.com>
2025-11-08 14:34:16 +0800

    Update TransMIT1.sh

--- TransMIT1.sh
+++ TransMIT1.sh
@@ -16,58 +16,58 @@ python -u run.py \
   --num_layers 2 \
   --num_heads 2
 
-# python -u run.py \
-#   --fix_seed $fix_seed \
-#   --data_name 'FTD' \
-#   --miss_rate 0.2 \
-#   --batch_size 100\
-#   --lr 0.0005 \
-#   --epochs 200 \
-#   --alpha 0.5 \
-#   --s 16 \
-#   --d_model 128 \
-#   --d_q 64 \
-#   --num_layers 4 \
-#   --num_heads 4
+python -u run.py \
+  --fix_seed $fix_seed \
+  --data_name 'FTD' \
+  --miss_rate 0.2 \
+  --batch_size 100\
+  --lr 0.0005 \
+  --epochs 200 \
+  --alpha 0.5 \
+  --s 16 \
+  --d_model 128 \
+  --d_q 64 \
+  --num_layers 4 \
+  --num_heads 4
 
-#   python -u run.py \
-#   --fix_seed $fix_seed \
-#   --data_name 'FTD' \
-#   --miss_rate 0.3 \
-#   --batch_size 100\
-#   --lr 0.0005 \
-#   --epochs 200 \
-#   --alpha 0.5 \
-#   --s 16 \
-#   --d_model 128 \
-#   --d_q 64 \
-#   --num_layers 4 \
-#   --num_heads 4
+  python -u run.py \
+  --fix_seed $fix_seed \
+  --data_name 'FTD' \
+  --miss_rate 0.3 \
+  --batch_size 100\
+  --lr 0.0005 \
+  --epochs 200 \
+  --alpha 0.5 \
+  --s 16 \
+  --d_model 128 \
+  --d_q 64 \
+  --num_layers 4 \
+  --num_heads 4
 
-# python -u run.py \
-#   --fix_seed $fix_seed \
-#   --data_name 'FTD' \
-#   --miss_rate 0.4 \
-#   --batch_size 100\
-#   --lr 0.0005 \
-#   --epochs 200 \
-#   --alpha 0.5 \
-#   --s 16 \
-#   --d_model 128 \
-#   --d_q 64 \
-#   --num_layers 4 \
-#   --num_heads 4
+python -u run.py \
+  --fix_seed $fix_seed \
+  --data_name 'FTD' \
+  --miss_rate 0.4 \
+  --batch_size 100\
+  --lr 0.0005 \
+  --epochs 200 \
+  --alpha 0.5 \
+  --s 16 \
+  --d_model 128 \
+  --d_q 64 \
+  --num_layers 4 \
+  --num_heads 4
 
-# python -u run.py \
-#   --fix_seed $fix_seed \
-#   --data_name 'FTD' \
-#   --miss_rate 0.5 \
-#   --batch_size 100\
-#   --lr 0.0005 \
-#   --epochs 200 \
-#   --alpha 0.5 \
-#   --s 16 \
-#   --d_model 128 \
-#   --d_q 64 \
-#   --num_layers 4 \
-#   --num_heads 4
+python -u run.py \
+  --fix_seed $fix_seed \
+  --data_name 'FTD' \
+  --miss_rate 0.5 \
+  --batch_size 100\
+  --lr 0.0005 \
+  --epochs 200 \
+  --alpha 0.5 \
+  --s 16 \
+  --d_model 128 \
+  --d_q 64 \
+  --num_layers 4 \
+  --num_heads 4
